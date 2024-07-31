0233e2411d73e227eb1a5dba1663585a48ee0275
amirhilal044 <amiramirhilal@gmail.com>
2024-07-31 18:27:10 +0300

    grouped routes

--- routes/api.php
+++ routes/api.php
@@ -1,6 +1,5 @@
 <?php
 
-use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\AuthController;
 use App\Http\Controllers\AdminController;
@@ -9,24 +8,45 @@ use App\Http\Controllers\SearchController;
 use App\Http\Controllers\FriendController;
 use App\Http\Controllers\CodeController;
 
+// Public routes
 Route::post('register', [AuthController::class, 'register']);
 Route::post('login', [AuthController::class, 'login']);
 Route::post('verify-email', [AuthController::class, 'verifyEmail']);
+
 // Protected routes
 Route::middleware('auth:api')->group(function () {
+
+    // Auth routes
     Route::post('refresh', [AuthController::class, 'refresh']);
     Route::get('validate-token', [AuthController::class, 'validateToken']);
     Route::post('logout', [AuthController::class, 'logout']);
     Route::get('me', [AuthController::class, 'me']);
+
+    // Chat routes
     Route::get('/chats/{userId}', [ChatController::class, 'getChats']);
     Route::post('/chats', [ChatController::class, 'sendMessage']);
     Route::get('/chat-users', [ChatController::class, 'getChatUsers']);
+
+    // Search route
     Route::get('/search-users', [SearchController::class, 'search']);
-    Route::post('/friend-requests', [FriendController::class, 'sendFriendRequest']);
-    Route::get('/friend-requests', [FriendController::class, 'getFriendRequests']);
-    Route::post('/friend-requests/{id}/accept', [FriendController::class, 'acceptFriendRequest']);
-    Route::post('/friend-requests/{id}/reject', [FriendController::class, 'rejectFriendRequest']);
+
+    // Friend request routes
+    Route::prefix('friend-requests')->group(function () {
+        Route::post('/', [FriendController::class, 'sendFriendRequest']);
+        Route::get('/', [FriendController::class, 'getFriendRequests']);
+        Route::post('/{id}/accept', [FriendController::class, 'acceptFriendRequest']);
+        Route::post('/{id}/reject', [FriendController::class, 'rejectFriendRequest']);
+    });
     Route::get('/friends', [FriendController::class, 'getFriends']);
+
+    // Code routes
+    Route::prefix('codes')->group(function () {
+        Route::post('/', [CodeController::class, 'store']);
+        Route::get('/', [CodeController::class, 'showAll']);
+        Route::get('/{id}', [CodeController::class, 'showOne']);
+        Route::put('/{id}', [CodeController::class, 'update']);
+        Route::delete('/{id}', [CodeController::class, 'destroy']);
+    });
 
     // Admin routes
     Route::middleware('admin')->prefix('admin')->group(function () {
@@ -35,10 +55,4 @@ Route::middleware('auth:api')->group(function () {
         Route::get('users', [AdminController::class, 'viewUsers']);
         Route::get('admins', [AdminController::class, 'viewAdmins']);
     });
-    Route::post('/codes', [CodeController::class, 'store']);
-    Route::get('/codes', [CodeController::class, 'showAll']);
-    Route::get('/codes/{id}', [CodeController::class, 'showOne']);
-    Route::put('/codes/{id}', [CodeController::class, 'update']);
-    Route::delete('/codes/{id}', [CodeController::class, 'destroy']);
 });
-
